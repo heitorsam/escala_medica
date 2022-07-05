ab5530536cb715607246097a108b8055571dbc67
CaioBecker <83302532+CaioBecker@users.noreply.github.com>
2022-07-05 17:23:31 -0300

    sem debounce

--- setor.php
+++ setor.php
@@ -38,7 +38,7 @@
     <div class="row">
         <div class="col-md-2">
             CRM:
-            <input type="text" onkeyup ="debounce(function() {campos_responsavel('1')}, 2000)" id="cd_responsavel" class="form-control" autocomplete="off"> 
+            <input type="text" onkeyup ="campos_responsavel('1')" id="cd_responsavel" class="form-control" autocomplete="off"> 
         </div>
         <div class="col-md-4">
             Responsável:
@@ -98,10 +98,6 @@
 <script>
 
     window.onload = function() { criar_tabela_setor(); document.getElementById('div_exame').style.display = "none"; };
-
-    const myInput = document.querySelector('input'),
-	mySpan = document.querySelector('span');
-    let counter = 0;
     
     function debounce(func, wait) {
         let timer = null;
